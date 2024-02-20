9e2d65a4434e8ec2bdc24b08964231c022478fa3
Pemakai2023 <150995191+Pemakai2023@users.noreply.github.com>
2024-02-20 18:36:09 +0800

    Update text.js

--- test/tests/text.js
+++ test/tests/text.js
@@ -1,5 +1,8 @@
 export default function text(ctx) {
-    ctx.font = "normal 120px Arial";
+    ctx.font = "normal 36px Arial";
     ctx.fillStyle = "#000000";
-    ctx.fillText("Hello", 0, 200);
+    ctx.fillText("A Text Example", 50, 50);
+    ctx.font = "normal 36px Arial";
+    ctx.strokeStyle = "#000000";
+    ctx.strokeText("A Text Example", 50, 90);
 };
